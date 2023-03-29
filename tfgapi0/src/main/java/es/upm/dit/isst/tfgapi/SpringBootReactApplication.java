75812b51e84d9eb1ceb25b98acac99e98d8dde04
lgcayetano <125416214+lgcayetano@users.noreply.github.com>
2023-03-29 21:13:22 +0200

    Primer Commit

--- tfgapi0/src/main/java/es/upm/dit/isst/tfgapi/SpringBootReactApplication.java
+++ tfgapi0/src/main/java/es/upm/dit/isst/tfgapi/SpringBootReactApplication.java
@@ -3,6 +3,7 @@ package es.upm.dit.isst.tfgapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//Prueba
 @SpringBootApplication
 public class SpringBootReactApplication {
     public static void main(String[] args) {
